47c33601f47e9b5c2e9e6e72be59761ff93d51da
Michael Merani <michaelmerani@michaels-macbook-pro.local>
2016-07-07 13:59:04 -0400

    Initial Commit

---
+++ Be_Helpful_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Be_Helpful_App
+//
+//  Created by Michael Merani on 7/7/16.
+//  Copyright © 2016 Michael Merani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
